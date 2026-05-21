42164eb5d082de4aff10311dadceb9030a63473f
sosokaizena1 <okotovac@gmail.com>
2026-05-21 21:21:15 +0300

    добавлена модель PopulationData для анализа населения

--- PopulationAndInflationAnalysis/Models/PopulationData.cs
+++ PopulationAndInflationAnalysis/Models/PopulationData.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace PopulationAndInflationAnalysis.Models
 {
+    /// <summary>
+    /// Модель данных о численности населения
+    /// </summary>
     public class PopulationData
     {
+        /// <summary>
+        /// Год, за который зафиксирована численность населения
+        /// </summary>
         public int Year { get; set; }
+
+        /// <summary>
+        /// Название региона (субъекта РФ)
+        /// </summary>
         public string Region { get; set; }
+
+        /// <summary>
+        /// Численность населения в миллионах человек
+        /// </summary>
         public double Population { get; set; }
     }
 }
