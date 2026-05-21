da5a6dc9781dae16ec0e7a0631173ef150939661
sosokaizena1 <okotovac@gmail.com>
2026-05-21 21:22:20 +0300

    добавлен сервис экспорта графиков в PNG

--- PopulationAndInflationAnalysis/Services/ChartExportService.cs
+++ PopulationAndInflationAnalysis/Services/ChartExportService.cs
@@ -4,17 +4,27 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PopulationAndInflationAnalysis.Services
 {
+    /// <summary>
+    /// Сервис для экспорта графиков в файлы изображений
+    /// </summary>
     public class ChartExportService
     {
+        /// <summary>
+        /// Экспортирует график из элемента Chart в PNG-изображение
+        /// </summary>
+        /// <param name="chart">Элемент управления Chart, содержащий график для экспорта</param>
+        /// <param name="filePath">Полный путь для сохранения файла </param>
         public void ExportToImage(Chart chart, string filePath)
         {
             try
             {
+                // Сохранение графика в PNG формате
                 chart.SaveImage(filePath, ChartImageFormat.Png);
                 MessageBox.Show($"График сохранён: {filePath}", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
+                // Обработка ошибок при сохранении
                 MessageBox.Show($"Ошибка сохранения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
