ea61bce14026a2b8c43a9ba2f073aa39d6f247a4
sosokaizena1 <okotovac@gmail.com>
2026-05-21 21:13:52 +0300

    Первая модель данных для варианта 10. Хранит информацию об инфляции по годам.

--- PopulationAndInflationAnalysis/Models/InflationData.cs
+++ PopulationAndInflationAnalysis/Models/InflationData.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace PopulationAndInflationAnalysis.Models
 {
+    /// <summary>
+    /// Модель данных об инфляции для варианта 10 лабораторной работы
+    /// </summary>
     public class InflationData
     {
+        /// <summary>
+        /// Год, за который зафиксирован показатель инфляции
+        /// </summary>
         public int Year { get; set; }
+
+        /// <summary>
+        /// Уровень инфляции в процентах за указанный год
+        /// </summary>
         public double InflationRate { get; set; }
     }
 }
